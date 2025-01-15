72de11b4de1fe73600e85b7f2639c06dc6520ab5
mitreb <tmitreb@gmail.com>
2025-01-15 20:56:40 +0100

    Add routes for Cart and Checkout functionality

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\CheckoutController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +23,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::post('/cart/{product}', [CartController::class, 'addToCart']);
+Route::post('/checkout', [CheckoutController::class, 'checkout']);
